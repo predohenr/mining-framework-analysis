10bb5fcf18c1c48a40c901f8834adbd611aa0885
Pedro Henrique <predohnr@gmail.com>
2026-04-13 23:40:19 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.rs

--- crates/proc-macro-srv/src/server_impl/rust_analyzer_span.rs
+++ crates/proc-macro-srv/src/server_impl/rust_analyzer_span.rs
@@ -178,11 +178,27 @@ impl server::Server for RaSpanServer<'_> {
         // FIXME requires db, returns the top level call site
         span
     }
-    fn span_byte_range(&mut self, span: Self::Span) -> Range<usize> {
-        if let Some(cb) = self.callback.as_mut() {
-            return cb.byte_range(span);
+
+    fn span_resolved_at(&mut self, span: Self::Span, at: Self::Span) -> Self::Span {
+        Span { ctx: at.ctx, ..span }
+    }
+    fn span_end(&mut self, span: Self::Span) -> Self::Span {
+        // We can't modify the span range for fixup spans, those are meaningful to fixup.
+        if span.anchor.ast_id == FIXUP_ERASED_FILE_AST_ID_MARKER {
+            return span;
         }
-        Range { start: span.range.start().into(), end: span.range.end().into() }
+        Span { range: TextRange::empty(span.range.end()), ..span }
+    }
+    fn span_start(&mut self, span: Self::Span) -> Self::Span {
+        // We can't modify the span range for fixup spans, those are meaningful to fixup.
+        if span.anchor.ast_id == FIXUP_ERASED_FILE_AST_ID_MARKER {
+            return span;
+        }
+        Span { range: TextRange::empty(span.range.start()), ..span }
+    }
+    fn span_column(&mut self, _span: Self::Span) -> usize {
+        // FIXME requires db to resolve line index, THIS IS NOT INCREMENTAL
+        1
     }
     fn span_join(&mut self, first: Self::Span, second: Self::Span) -> Option<Self::Span> {
         // We can't modify the span range for fixup spans, those are meaningful to fixup, so just
@@ -255,33 +271,26 @@ impl server::Server for RaSpanServer<'_> {
             ..span
         })
     }
-
-    fn span_resolved_at(&mut self, span: Self::Span, at: Self::Span) -> Self::Span {
-        Span { ctx: at.ctx, ..span }
+    fn span_line(&mut self, _span: Self::Span) -> usize {
+        // FIXME requires db to resolve line index, THIS IS NOT INCREMENTAL
+        1
     }
-
-    fn span_end(&mut self, span: Self::Span) -> Self::Span {
-        // We can't modify the span range for fixup spans, those are meaningful to fixup.
-        if span.anchor.ast_id == FIXUP_ERASED_FILE_AST_ID_MARKER {
-            return span;
-        }
-        Span { range: TextRange::empty(span.range.end()), ..span }
+    fn span_byte_range(&mut self, span: Self::Span) -> Range<usize> {
+        // FIXME requires db to resolve the ast id, THIS IS NOT INCREMENTAL
+        Range { start: span.range.start().into(), end: span.range.end().into() }
     }
-
-    fn span_start(&mut self, span: Self::Span) -> Self::Span {
-        // We can't modify the span range for fixup spans, those are meaningful to fixup.
-        if span.anchor.ast_id == FIXUP_ERASED_FILE_AST_ID_MARKER {
-            return span;
-        }
-        Span { range: TextRange::empty(span.range.start()), ..span }
-    }
-
-    fn span_line(&mut self, span: Self::Span) -> usize {
-        self.callback.as_mut().and_then(|cb| cb.line_column(span)).map_or(1, |(l, _)| l as usize)
-    }
-
-    fn span_column(&mut self, span: Self::Span) -> usize {
+    fn column(&mut self, span: Self::Span) -> usize {
         self.callback.as_mut().and_then(|cb| cb.line_column(span)).map_or(1, |(_, c)| c as usize)
+    }
+    fn byte_range(&mut self, span: Self::Span) -> Range<usize> {
+        if let Some(cb) = self.callback.as_mut() {
+            return cb.byte_range(span);
+        }
+
+        Range { start: span.range.start().into(), end: span.range.end().into() }
+    }
+    fn line(&mut self, span: Self::Span) -> usize {
+        self.callback.as_mut().and_then(|cb| cb.line_column(span)).map_or(1, |(l, _)| l as usize)
     }
 
     fn symbol_normalize_and_validate_ident(&mut self, string: &str) -> Result<Self::Symbol, ()> {
