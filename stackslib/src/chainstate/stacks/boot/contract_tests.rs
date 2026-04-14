23228942826ddc713034c037e71c2073556bef76
Pedro Henrique <predohnr@gmail.com>
2026-04-14 23:52:57 +0000

    Mining Framework Analysis for merge.mergiraf.rs

--- stackslib/src/chainstate/stacks/boot/contract_tests.rs
+++ stackslib/src/chainstate/stacks/boot/contract_tests.rs
@@ -32,9 +32,7 @@ use crate::chainstate::stacks::boot::{
 use crate::chainstate::stacks::index::ClarityMarfTrieId;
 use crate::chainstate::stacks::{C32_ADDRESS_VERSION_TESTNET_SINGLESIG, *};
 use crate::clarity_vm::clarity::{
-    ClarityBlockConnection, ClarityError, ClarityMarfStore, ClarityMarfStoreTransaction,
-    WritableMarfStore,
-};
+    ClarityBlockConnection, ClarityMarfStore, ClarityError, WritableMarfStore, ClarityMarfStoreTransaction, };
 use crate::clarity_vm::database::marf::MarfedKV;
 use crate::core::{
     StacksEpoch, StacksEpochId, BITCOIN_REGTEST_FIRST_BLOCK_HASH,
