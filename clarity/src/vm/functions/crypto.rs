31dad712f7129e59e43af66d91daeef5dd90aeee
Pedro Henrique <predohnr@gmail.com>
2026-04-14 18:12:31 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.rs

--- clarity/src/vm/functions/crypto.rs
+++ clarity/src/vm/functions/crypto.rs
@@ -20,6 +20,7 @@ use stacks_common::address::{
 use stacks_common::types::chainstate::StacksAddress;
 use stacks_common::util::hash;
 use stacks_common::util::secp256k1::{Secp256k1PublicKey, secp256k1_recover, secp256k1_verify};
+use stacks_common::util::secp256k1::{secp256k1_recover, secp256k1_verify, Secp256k1PublicKey};
 use stacks_common::util::secp256r1::{secp256r1_verify, secp256r1_verify_digest};
 
 use crate::vm::costs::cost_functions::ClarityCostFunction;
