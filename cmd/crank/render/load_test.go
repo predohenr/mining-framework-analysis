8903f69374eb6e7c87fc6ba80b189c4da90d230e
Pedro Henrique <predohnr@gmail.com>
2026-04-15 17:08:03 +0000

    Mining Framework Analysis for merge.mergiraf.go

--- cmd/crank/render/load_test.go
+++ cmd/crank/render/load_test.go
@@ -32,10 +32,10 @@ import (
 
 	apiextensionsv1 "github.com/crossplane/crossplane/apis/apiextensions/v1"
 	pkgv1 "github.com/crossplane/crossplane/apis/pkg/v1"
-	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	runtime "k8s.io/apimachinery/pkg/runtime"
-	"github.com/crossplane/crossplane/internal/xresource/unstructured/composed"
 	"github.com/crossplane/crossplane/internal/xresource/unstructured/composite"
+	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"github.com/crossplane/crossplane/internal/xresource/unstructured/composed"
+	runtime "k8s.io/apimachinery/pkg/runtime"
 )
 
 //go:embed testdata
