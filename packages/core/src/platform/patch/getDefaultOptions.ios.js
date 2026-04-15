38ed5cffb4a1e550ed9f82bc6ac33680d12636bb
Pedro Henrique <predohnr@gmail.com>
2026-04-15 10:30:28 +0000

    Mining Framework Analysis for merge.mergiraf.js

--- packages/core/src/platform/patch/getDefaultOptions.ios.js
+++ packages/core/src/platform/patch/getDefaultOptions.ios.js
@@ -11,10 +11,10 @@ import { queueJob, hasPendingJob } from '../../observer/scheduler'
 import { createSelectorQuery, createIntersectionObserver } from '@mpxjs/api-proxy'
 import { IntersectionObserverContext, RouteContext, KeyboardAvoidContext } from '@mpxjs/webpack-plugin/lib/runtime/components/react/dist/context'
 import MpxKeyboardAvoidingView from '@mpxjs/webpack-plugin/lib/runtime/components/react/dist/mpx-keyboard-avoiding-view'
-
-const ProviderContext = createContext(null)
 const windowDimensions = ReactNative.Dimensions.get('window')
 const screenDimensions = ReactNative.Dimensions.get('screen')
+const ProviderContext = createContext(null)
+
 function getSystemInfo () {
   return {
     deviceOrientation: windowDimensions.width > windowDimensions.height ? 'landscape' : 'portrait',
