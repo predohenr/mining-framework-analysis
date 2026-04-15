d3a39cfb1f5ff73a23cad1da78a6a77e03044fec
Pedro Henrique <predohnr@gmail.com>
2026-04-15 10:36:39 +0000

    Mining Framework Analysis for merge.mergiraf.js

--- packages/core/src/platform/builtInMixins/styleHelperMixin.ios.js
+++ packages/core/src/platform/builtInMixins/styleHelperMixin.ios.js
@@ -1,5 +1,6 @@
 import { isObject, isArray, dash2hump, cached, isEmptyObject } from '@mpxjs/utils'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, useWindowDimensions } from 'react-native'
+import { REACTHOOKSEXEC, useEffect } from '../../core/innerLifecycle'
 
 function rpx (value, windowInfo) {
   // rn 单位 dp = 1(css)px =  1 物理像素 * pixelRatio(像素比)
@@ -171,13 +172,13 @@ export default function styleHelperMixin () {
           const classString = mpEscape(concat(staticClass, stringifyDynamicClass(dynamicClass)))
           classString.split(/\s+/).forEach((className) => {
             if (classMap[className]) {
-              // const styleObj = classMap[className]
-              // if (styleObj.normal) {
-              //   const { width } = useWindowDimensions()
-              //   console.log(width, 999000)
-              // } else {
-              //   Object.assign(result, classMap[className])
-              // }
+              const styleObj = classMap[className]
+              if (styleObj.normal) {
+                const { width } = useWindowDimensions()
+                console.log(width, 999000)
+              } else {
+                Object.assign(result, classMap[className])
+              }
               Object.assign(result, classMap[className])
             } else if (appClassMap[className]) {
               // todo 全局样式在每个页面和组件中生效，以支持全局原子类，后续支持样式模块复用后可考虑移除
