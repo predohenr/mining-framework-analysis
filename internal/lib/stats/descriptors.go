4b69c14b1791a3882ee61210fcdb3f215f4237df
Pedro Henrique <predohnr@gmail.com>
2026-04-15 16:58:19 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.go

--- internal/lib/stats/descriptors.go
+++ internal/lib/stats/descriptors.go
@@ -293,4 +293,9 @@ var (
 		Help:      "Start time of the container since unix epoch in seconds.",
 		LabelKeys: baseLabelKeys,
 	}
+	containerStartTimeSeconds = &types.MetricDescriptor{
+		Name:      "container_start_time_seconds",
+		Help:      "Start time of the container since unix epoch in seconds.",
+		LabelKeys: baseLabelKeys,
+	}
 )
