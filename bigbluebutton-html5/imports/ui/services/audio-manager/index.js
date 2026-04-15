f4539b797d40df5481e98ab0ed7e136dbedae5cf
Pedro Henrique <predohnr@gmail.com>
2026-04-15 09:21:54 +0000

    Mining Framework Analysis for merge.mergiraf.js

--- bigbluebutton-html5/imports/ui/services/audio-manager/index.js
+++ bigbluebutton-html5/imports/ui/services/audio-manager/index.js
@@ -224,14 +224,14 @@ class AudioManager {
       this.fullAudioBridge.inputDeviceId = this._inputDeviceId.value();
     }
   }
+  get inputDeviceId() {
+    return this._inputDeviceId.value();
+  }
 
   // inputDeviceId is a string that represents a MediaDeviceInfo.deviceId OR a static
   // 'listen-only' string that represents our "virtual" listen-only device.
   // i.e.: the user has a bidirectional audio channel, but did not specify any
   // input device to it.
-  get inputDeviceId() {
-    return this._inputDeviceId.value();
-  }
 
   set outputDeviceId(value) {
     if (this._outputDeviceId.value() !== value) {
@@ -1139,10 +1139,6 @@ class AudioManager {
     return this._inputStream();
   }
 
-  get bridge() {
-    return this.isListenOnly ? this.listenOnlyBridge : this.fullAudioBridge;
-  }
-
   set inputStream(stream) {
     const previousStream = this.inputStream;
 
@@ -1162,6 +1158,10 @@ class AudioManager {
     }
 
     this.trackStreamTermination(stream);
+  }
+
+  get bridge() {
+    return this.isListenOnly ? this.listenOnlyBridge : this.fullAudioBridge;
   }
 
   /**
