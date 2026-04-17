b2fc83203b76fcbc2b1163808afc4ab652846c8c
Pedro Henrique <predohnr@gmail.com>
2026-04-17 09:52:08 +0000

    Mining Framework Analysis for merge.mergiraf_semi.py

--- kedro/io/core.py
+++ kedro/io/core.py
@@ -786,13 +786,51 @@ class AbstractVersionedDataset(AbstractDataset[_DI, _DO], abc.ABC):
             message = f"Did not find any versions for {self}"
             raise VersionNotFoundError(message)
         return PurePath(most_recent).parent.name
-
-    # 'key' is set to prevent cache key overlapping for load and save:
-    # https://cachetools.readthedocs.io/en/stable/#cachetools.cachedmethod
     @cachedmethod(cache=attrgetter("_version_cache"), key=partial(hashkey, "save"))
     def _fetch_latest_save_version(self) -> str:
         """Generate and cache the current save version"""
         return generate_timestamp()
+
+    @classmethod
+    def _save_wrapper(
+        cls, save_func: Callable[[Self, _DI], None]
+    ) -> Callable[[Self, _DI], None]:
+        """Decorate `save_func` with logging and error handling code."""
+
+        @wraps(save_func)
+        def save(self: Self, data: _DI) -> None:
+            self._version_cache.clear()
+            save_version = (
+                self.resolve_save_version()
+            )  # Make sure last save version is set
+            try:
+                super()._save_wrapper(save_func)(self, data)
+            except (FileNotFoundError, NotADirectoryError) as err:
+                # FileNotFoundError raised in Win, NotADirectoryError raised in Unix
+                _default_version = "YYYY-MM-DDThh.mm.ss.sssZ"
+                raise DatasetError(
+                    f"Cannot save versioned dataset '{self._filepath.name}' to "
+                    f"'{self._filepath.parent.as_posix()}' because a file with the same "
+                    f"name already exists in the directory. This is likely because "
+                    f"versioning was enabled on a dataset already saved previously. Either "
+                    f"remove '{self._filepath.name}' from the directory or manually "
+                    f"convert it into a versioned dataset by placing it in a versioned "
+                    f"directory (e.g. with default versioning format "
+                    f"'{self._filepath.as_posix()}/{_default_version}/{self._filepath.name}"
+                    f"')."
+                ) from err
+
+            load_version = self.resolve_load_version()
+            if load_version != save_version:
+                warnings.warn(
+                    _CONSISTENCY_WARNING.format(save_version, load_version, str(self))
+                )
+                self._version_cache.clear()
+
+        return save
+
+    # 'key' is set to prevent cache key overlapping for load and save:
+    # https://cachetools.readthedocs.io/en/stable/#cachetools.cachedmethod
 
     def resolve_load_version(self) -> str | None:
         """Compute the version the dataset should be loaded with."""
@@ -836,44 +874,6 @@ class AbstractVersionedDataset(AbstractDataset[_DI, _DO], abc.ABC):
 
     def _get_versioned_path(self, version: str) -> PurePosixPath:
         return self._filepath / version / self._filepath.name
-
-    @classmethod
-    def _save_wrapper(
-        cls, save_func: Callable[[Self, _DI], None]
-    ) -> Callable[[Self, _DI], None]:
-        """Decorate `save_func` with logging and error handling code."""
-
-        @wraps(save_func)
-        def save(self: Self, data: _DI) -> None:
-            self._version_cache.clear()
-            save_version = (
-                self.resolve_save_version()
-            )  # Make sure last save version is set
-            try:
-                super()._save_wrapper(save_func)(self, data)
-            except (FileNotFoundError, NotADirectoryError) as err:
-                # FileNotFoundError raised in Win, NotADirectoryError raised in Unix
-                _default_version = "YYYY-MM-DDThh.mm.ss.sssZ"
-                raise DatasetError(
-                    f"Cannot save versioned dataset '{self._filepath.name}' to "
-                    f"'{self._filepath.parent.as_posix()}' because a file with the same "
-                    f"name already exists in the directory. This is likely because "
-                    f"versioning was enabled on a dataset already saved previously. Either "
-                    f"remove '{self._filepath.name}' from the directory or manually "
-                    f"convert it into a versioned dataset by placing it in a versioned "
-                    f"directory (e.g. with default versioning format "
-                    f"'{self._filepath.as_posix()}/{_default_version}/{self._filepath.name}"
-                    f"')."
-                ) from err
-
-            load_version = self.resolve_load_version()
-            if load_version != save_version:
-                warnings.warn(
-                    _CONSISTENCY_WARNING.format(save_version, load_version, str(self))
-                )
-                self._version_cache.clear()
-
-        return save
 
     def exists(self) -> bool:
         """Checks whether a dataset's output already exists by calling
