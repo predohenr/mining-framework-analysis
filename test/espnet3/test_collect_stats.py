3534b27cee47389c13da379eb8f599d60ec59759
Pedro Henrique <predohnr@gmail.com>
2026-04-17 02:07:17 +0000

    Mining Framework Analysis for merge.mergiraf.py

--- test/espnet3/test_collect_stats.py
+++ test/espnet3/test_collect_stats.py
@@ -9,7 +9,7 @@ from hydra.utils import instantiate
 from omegaconf import OmegaConf
 
 # Import the functions under test (adjust import path if your file/module path differs)
-from espnet3.base.collect_stats import collect_stats
+from espnet3.base.collect_stats import collect_stats, collect_stats_multiple_iterator
 
 mp.set_start_method("fork", force=True)
 
