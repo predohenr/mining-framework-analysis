0931912daeb94eef98131ba96b920e437fb80042
Pedro Henrique <predohnr@gmail.com>
2026-04-17 10:54:38 +0000

    Mining Framework Analysis for merge.mergiraf.py

--- spyder/plugins/editor/confpage.py
+++ spyder/plugins/editor/confpage.py
@@ -6,20 +6,17 @@
 
 """Editor config page."""
 
+from spyder.api.config.decorators import on_conf_change
+from spyder.api.preferences import PluginConfigPage
+from spyder.widgets.helperwidgets import TipWidget
+from qtpy.QtCore import Qt, Signal
+from spyder.config.base import _
+from spyder.api.config.mixins import SpyderConfigurationObserver
 from itertools import combinations
-
+from spyder.config.manager import CONF
 from qtpy.QtWidgets import (QGridLayout, QGroupBox, QHBoxLayout, QLabel,
                             QVBoxLayout, QDialog, QDialogButtonBox, QWidget,
                             QCheckBox, QSizePolicy)
-from qtpy.QtCore import Qt, Signal
-
-from spyder.api.config.decorators import on_conf_change
-from spyder.api.config.mixins import SpyderConfigurationObserver
-from spyder.api.preferences import PluginConfigPage
-from spyder.config.base import _
-from spyder.config.manager import CONF
-from spyder.utils.icon_manager import ima
-from spyder.widgets.helperwidgets import TipWidget
 
 
 NUMPYDOC = "https://numpydoc.readthedocs.io/en/latest/format.html"
