e6590f72bc19acfdd537223e57e392edf4c9f27a
Pedro Henrique <predohnr@gmail.com>
2026-04-17 06:00:16 +0000

    Mining Framework Analysis for merge.mergiraf_semi.py

--- lutris/gui/lutriswindow.py
+++ lutris/gui/lutriswindow.py
@@ -7,18 +7,16 @@ from collections import namedtuple
 from datetime import datetime
 from gettext import gettext as _
 from gettext import ngettext
-from typing import Dict, Iterable, List, Set, cast
-from urllib.parse import unquote, urlparse
-
 from gi.repository import Gdk, Gio, GLib, Gtk
-
-from lutris import services, settings
+from lutris.database import categories
 from lutris.api import (
     LUTRIS_ACCOUNT_CONNECTED,
     LUTRIS_ACCOUNT_DISCONNECTED,
     get_runtime_versions,
 )
-from lutris.database import categories
+from urllib.parse import unquote, urlparse
+from lutris import services, settings
+from typing import Dict, Iterable, List, Set, cast
 from lutris.database import categories as categories_db
 from lutris.database import games as games_db
 from lutris.database import saved_searches as saved_searches_db
