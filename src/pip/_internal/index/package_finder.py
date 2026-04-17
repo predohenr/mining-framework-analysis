aef91ff1daf6a2e28f98296b97d2d6e06653fe1f
Pedro Henrique <predohnr@gmail.com>
2026-04-17 09:03:12 +0000

    Mining Framework Analysis for merge.mergiraf_semi.py

--- src/pip/_internal/index/package_finder.py
+++ src/pip/_internal/index/package_finder.py
@@ -1,27 +1,27 @@
 """Routines related to PyPI, indexes"""
 
 from __future__ import annotations
-
-import datetime
-import enum
-import functools
-import itertools
-import logging
-import re
-from collections.abc import Iterable
-from dataclasses import dataclass
 from typing import (
     TYPE_CHECKING,
     Optional,
     Union,
 )
-
-from pip._vendor.packaging import specifiers
-from pip._vendor.packaging.tags import Tag
-from pip._vendor.packaging.utils import NormalizedName, canonicalize_name
+import logging
+from collections.abc import Iterable
+import functools
 from pip._vendor.packaging.version import InvalidVersion, Version, _BaseVersion
 from pip._vendor.packaging.version import parse as parse_version
-
+from pip._internal.metadata import select_backend
+import itertools
+from dataclasses import dataclass
+import enum
+from pip._vendor.packaging.utils import NormalizedName, canonicalize_name
+import re
+from pip._vendor.packaging.tags import Tag
+from pip._vendor.packaging import specifiers
+from pip._vendor.packaging.version import InvalidVersion, _BaseVersion
+import datetime
+from pip._internal.index.collector import IndexContent, LinkCollector, parse_links
 from pip._internal.exceptions import (
     BestVersionAlreadyInstalled,
     DistributionNotFound,
@@ -29,8 +29,6 @@ from pip._internal.exceptions import (
     InvalidWheelFilename,
     UnsupportedWheel,
 )
-from pip._internal.index.collector import IndexContent, LinkCollector, parse_links
-from pip._internal.metadata import select_backend
 from pip._internal.models.candidate import InstallationCandidate
 from pip._internal.models.format_control import FormatControl
 from pip._internal.models.link import Link
