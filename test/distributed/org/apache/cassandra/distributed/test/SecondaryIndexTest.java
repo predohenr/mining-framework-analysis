4c9f0ca983a0734224ae2aae774f81c7a60fd7ee
Pedro Henrique <predohnr@gmail.com>
2026-04-18 18:07:00 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.java

--- test/distributed/org/apache/cassandra/distributed/test/SecondaryIndexTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/SecondaryIndexTest.java
@@ -35,7 +35,6 @@ import java.util.stream.Collectors;
 
 import com.google.common.collect.Sets;
 
-import org.awaitility.Awaitility;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -51,6 +50,7 @@ import org.apache.cassandra.distributed.api.ConsistencyLevel;
 import org.apache.cassandra.utils.TimeUUID;
 
 import static org.apache.cassandra.distributed.impl.IsolatedExecutor.waitOn;
+import org.awaitility.Awaitility;
 
 public class SecondaryIndexTest extends TestBaseImpl
 {
