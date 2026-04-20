9fffe86bc055a77e2ad54b6193ca6517d7836945
Pedro Henrique <predohnr@gmail.com>
2026-04-20 14:11:49 +0000

    Mining Framework Analysis for merge.mergiraf_semi.java

--- integrationtests/src/test/java/org/axonframework/integrationtests/queryhandling/SimpleQueryBusSubscriptionQueryTest.java
+++ integrationtests/src/test/java/org/axonframework/integrationtests/queryhandling/SimpleQueryBusSubscriptionQueryTest.java
@@ -18,17 +18,17 @@ package org.axonframework.integrationtests.queryhandling;
 
 import org.axonframework.axonserver.connector.AxonServerConfigurationEnhancer;
 import org.axonframework.common.configuration.Configuration;
-import org.axonframework.messaging.core.MessageStream;
-import org.axonframework.messaging.core.configuration.MessagingConfigurer;
-import org.axonframework.messaging.queryhandling.GenericQueryMessage;
-import org.axonframework.messaging.queryhandling.QueryBus;
-import org.axonframework.messaging.queryhandling.QueryMessage;
-import org.axonframework.messaging.queryhandling.QueryResponseMessage;
-import org.axonframework.messaging.queryhandling.SimpleQueryBus;
-import org.axonframework.messaging.queryhandling.SubscriptionQueryAlreadyRegisteredException;
-import org.junit.jupiter.api.*;
-
+import org.axonframework.queryhandling.SubscriptionQueryAlreadyRegisteredException;
 import static org.junit.jupiter.api.Assertions.*;
+import org.axonframework.queryhandling.QueryMessage;
+import org.axonframework.common.configuration.MessagingConfigurer;
+import org.axonframework.messaging.MessageStream;
+import org.axonframework.queryhandling.GenericQueryMessage;
+import org.axonframework.queryhandling.QueryResponseMessage;
+import org.junit.jupiter.api.*;
+import org.axonframework.messaging.core.configuration.MessagingConfigurer;
+import org.axonframework.messaging.queryhandling.SimpleQueryBus;
+import org.axonframework.messaging.queryhandling.QueryBus;
 
 /**
  * An {@link AbstractSubscriptionQueryTestSuite} implementation validating the {@link SimpleQueryBus}.
