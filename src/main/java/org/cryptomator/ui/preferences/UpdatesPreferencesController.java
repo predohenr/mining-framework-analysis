ec90db9ad22b5281c4a131253abe3a4c78f1fb49
Pedro Henrique <predohnr@gmail.com>
2026-04-24 06:44:26 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.java

--- src/main/java/org/cryptomator/ui/preferences/UpdatesPreferencesController.java
+++ src/main/java/org/cryptomator/ui/preferences/UpdatesPreferencesController.java
@@ -2,8 +2,11 @@ package org.cryptomator.ui.preferences;
 
 import org.cryptomator.common.Environment;
 import org.cryptomator.common.settings.Settings;
-import org.cryptomator.integrations.update.UpdateMechanism;
 import org.cryptomator.integrations.update.UpdateProcess;
+import org.cryptomator.integrations.update.UpdateMechanism;
+import org.cryptomator.integrations.update.UpdateFailedException;
+import org.cryptomator.common.updates.AppUpdateChecker;
+import org.cryptomator.integrations.common.DisplayName;
 import org.cryptomator.ui.common.FxController;
 import org.cryptomator.ui.fxapp.UpdateChecker;
 import org.slf4j.Logger;
@@ -18,14 +21,18 @@ import javafx.beans.binding.BooleanBinding;
 import javafx.beans.binding.ObjectBinding;
 import javafx.beans.binding.StringBinding;
 import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ContentDisplay;
 import java.io.IOException;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
@@ -48,6 +55,7 @@ public class UpdatesPreferencesController implements FxController {
 			+ "?utm_source=cryptomator-desktop" //
 			+ "&utm_medium=update-notification&" //
 			+ "utm_campaign=app-update-%s";
+	private static final String DISPLAY_NAME_FLATPAK = "Update via Flatpak update";
 
 	private final Application application;
 	private final Environment environment;
@@ -55,6 +63,7 @@ public class UpdatesPreferencesController implements FxController {
 	private final Settings settings;
 	private final Environment env;
 	private final UpdateChecker updateChecker;
+	private final AppUpdateChecker appUpdateChecker;
 	private final ObjectBinding<ContentDisplay> checkForUpdatesButtonState;
 	private final ReadOnlyStringProperty latestVersion;
 	private final ObservableValue<Instant> lastSuccessfulUpdateCheck;
@@ -62,32 +71,38 @@ public class UpdatesPreferencesController implements FxController {
 	private final ObservableValue<String> timeDifferenceMessage;
 	private final String currentVersion;
 	private final BooleanBinding updateAvailable;
+	private final BooleanBinding appUpdateAvailable;
 	private final BooleanBinding checkFailed;
 	private final BooleanProperty upToDateLabelVisible = new SimpleBooleanProperty(false);
 	private final DateTimeFormatter formatter;
 	private final BooleanBinding upToDate;
 	private final String downloadsUri;
-	private final UpdateMechanism updateMechanism;
 	public final Task<UpdateProcess> updatePreparationTask;
+	private final UpdateMechanism updateMechanism;
 	private final StringBinding updateButtonTitle;
+	private final DoubleProperty flatpakProgress = new SimpleDoubleProperty(ProgressBar.INDETERMINATE_PROGRESS);
+	private final BooleanProperty updatingFlatpak = new SimpleBooleanProperty(false);
 
 	/* FXML */
 	public CheckBox checkForUpdatesCheckbox;
+	public Label flatpakButtonLabel;
 
 	@Inject
-	UpdatesPreferencesController(Application application, Environment environment, ResourceBundle resourceBundle, Settings settings, UpdateChecker updateChecker, Environment env) {
+	UpdatesPreferencesController(Application application, Environment environment, ResourceBundle resourceBundle, Settings settings, UpdateChecker updateChecker, AppUpdateChecker appUpdateChecker, Environment env) {
 		this.application = application;
 		this.environment = environment;
 		this.resourceBundle = resourceBundle;
 		this.settings = settings;
 		this.env = env;
 		this.updateChecker = updateChecker;
+		this.appUpdateChecker = appUpdateChecker;
 		this.checkForUpdatesButtonState = Bindings.when(updateChecker.checkingForUpdatesProperty()).then(ContentDisplay.LEFT).otherwise(ContentDisplay.TEXT_ONLY);
 		this.latestVersion = updateChecker.latestVersionProperty();
 		this.lastSuccessfulUpdateCheck = updateChecker.lastSuccessfulUpdateCheckProperty();
 		this.timeDifferenceMessage = Bindings.createStringBinding(this::getTimeDifferenceMessage, lastSuccessfulUpdateCheck);
 		this.currentVersion = environment.getAppVersion();
 		this.updateAvailable = updateChecker.updateAvailableProperty();
+		this.appUpdateAvailable = updateChecker.appUpdateAvailableProperty();
 		this.formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withLocale(Locale.getDefault());
 		this.upToDate = updateChecker.updateCheckStateProperty().isEqualTo(UpdateChecker.UpdateCheckState.CHECK_SUCCESSFUL).and(latestVersion.isEqualTo(currentVersion));
 		this.checkFailed = updateChecker.checkFailedProperty();
@@ -109,6 +124,11 @@ public class UpdatesPreferencesController implements FxController {
 
 	public void initialize() {
 		checkForUpdatesCheckbox.selectedProperty().bindBidirectional(settings.checkForUpdates);
+		switch (env.getBuildNumber().get()) {
+			case "flatpak-1" -> flatpakButtonLabel.setText(appUpdateChecker.getServiceForChannel(DISPLAY_NAME_FLATPAK).getClass().getAnnotation(DisplayName.class).value());
+			default -> LOG.error("Unexpected value 'buildNumber': {}", env.getBuildNumber().get());
+		}
+
 		upToDate.addListener((_, _, newVal) -> {
 			if (newVal) {
 				upToDateLabelVisible.set(true);
@@ -125,6 +145,20 @@ public class UpdatesPreferencesController implements FxController {
 	}
 
 	@FXML
+	public void updateFlatpakNow() {
+		updatingFlatpak.set(true);
+		updateChecker.terminateFlatpakOnUpdateCompleted(
+				() -> updatingFlatpak.set(false), this
+		);
+
+		try {
+			updateChecker.updateAppNow();
+		} catch (UpdateFailedException e) {
+			updatingFlatpak.set(false);
+		}
+	}
+
+	@FXML
 	public void visitDownloadsPage() {
 		application.getHostServices().showDocument(downloadsUri);
 	}
@@ -135,7 +169,7 @@ public class UpdatesPreferencesController implements FxController {
 	}
 
 	@FXML
-	public void doUpdate() {
+	public void prepareUpdate() {
 		if (updatePreparationTask.isDone()) {
 			try {
 				// TODO: check if all vaults closed?
@@ -224,8 +258,16 @@ public class UpdatesPreferencesController implements FxController {
 		return updateAvailable;
 	}
 
+	public BooleanBinding appUdateAvailableProperty() {
+		return appUpdateAvailable;
+	}
+
 	public boolean isUpdateAvailable() {
 		return updateAvailable.get();
+	}
+
+	public boolean isAppUpdateAvailable() {
+		return appUpdateAvailable.get();
 	}
 
 	public BooleanBinding checkFailedProperty() {
@@ -246,10 +288,26 @@ public class UpdatesPreferencesController implements FxController {
 
 	public String getUpdateButtonTitle() {
 		return switch (updatePreparationTask.getState()) {
-			case READY -> updateMechanism.getName();
-			case SCHEDULED, RUNNING -> "Preparing Update..."; // TODO: resourceBundle.getString("preferences.updates.preparingUpdate")...
-			case SUCCEEDED -> "Restart to Update"; // TODO: resourceBundle.getString("preferences.updates.readyToRestart")...
+			case READY -> "Prepare Update"; // TODO: resourceBundle.getString("preferences.updates.preparingUpdate")...
+			case SCHEDULED, RUNNING -> "Preparing Update...";
+			case SUCCEEDED -> "Restart to Update";
 			case FAILED, CANCELLED -> "failed";
 		};
+	}
+
+	public BooleanProperty updatingFlatpakProperty() {
+		return updatingFlatpak;
+	}
+
+	public boolean isUpdatingFlatpak() {
+		return updatingFlatpak.get();
+	}
+
+	public DoubleProperty flatpakProgressProperty() {
+		return flatpakProgress;
+	}
+
+	public double getFlatpakProgress() {
+		return flatpakProgress.get();
 	}
 }
