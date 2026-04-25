2c9a7de06ad9c2c02c5f094482f1f67518126e4a
Pedro Henrique <predohnr@gmail.com>
2026-04-25 20:38:59 +0000

    Mining Framework Analysis for merge.mergiraf_semi.java

--- test/unit/org/apache/cassandra/schema/CreateTableValidationTest.java
+++ test/unit/org/apache/cassandra/schema/CreateTableValidationTest.java
@@ -134,17 +134,23 @@ public class CreateTableValidationTest extends CQLTester
                              String.format("CREATE TABLE %s.\"    \" (key int PRIMARY KEY, val int)", KEYSPACE));
     }
 
-    @Test
-    public void testInvalidCompactionOptions()
-    {
-        expectedFailure(ConfigurationException.class, "CREATE TABLE %s (k int PRIMARY KEY, v int) WITH compaction = {'class': 'LeveledCompactionStrategy', 'fanout_size': '90', 'sstable_size_in_mb': '1089'}",
-                        "your maxSSTableSize must be absurdly high to compute");
-    }
-
     private void expectedFailure(final Class<? extends RequestValidationException> exceptionType, String statement, String errorMsg)
     {
 
         assertThatExceptionOfType(exceptionType)
         .isThrownBy(() -> createTableMayThrow(statement)) .withMessageContaining(errorMsg);
+    }
+
+    private void expectedFailure(String statement, String errorMsg)
+    {
+        assertThatExceptionOfType(InvalidRequestException.class)
+        .isThrownBy(() -> createTableMayThrow(statement)) .withMessageContaining(errorMsg);
+    }
+
+    @Test
+    public void testInvalidCompactionOptions()
+    {
+        expectedFailure(ConfigurationException.class, "CREATE TABLE %s (k int PRIMARY KEY, v int) WITH compaction = {'class': 'LeveledCompactionStrategy', 'fanout_size': '90', 'sstable_size_in_mb': '1089'}",
+                        "your maxSSTableSize must be absurdly high to compute");
     }
 }
