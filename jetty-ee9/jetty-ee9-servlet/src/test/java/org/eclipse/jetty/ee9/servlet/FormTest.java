1f15a82f01bf84b3a155a232b4ab912a74698415
Pedro Henrique <predohnr@gmail.com>
2026-04-26 12:04:16 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.java

--- jetty-ee9/jetty-ee9-servlet/src/test/java/org/eclipse/jetty/ee9/servlet/FormTest.java
+++ jetty-ee9/jetty-ee9-servlet/src/test/java/org/eclipse/jetty/ee9/servlet/FormTest.java
@@ -111,6 +111,19 @@ public class FormTest
         );
     }
 
+    public static Stream<Integer> formKeysScenarios()
+    {
+        return Stream.of(null, -1, 0, MAX_FORM_KEYS);
+    }
+
+    private AsyncRequestContent newContent(int size)
+    {
+        byte[] key = "foo=".getBytes(StandardCharsets.US_ASCII);
+        byte[] value = new byte[size - key.length];
+        Arrays.fill(value, (byte)'x');
+        return new AsyncRequestContent(ByteBuffer.wrap(key), ByteBuffer.wrap(value));
+    }
+
     @ParameterizedTest
     @MethodSource("formContentSizeScenarios")
     public void testMaxFormContentSizeExceeded(Integer maxFormContentSize, Integer contentSize, boolean withContentLength, int expectedStatus) throws Exception
@@ -153,17 +166,66 @@ public class FormTest
         assertEquals(expectedStatus, response.getStatus());
     }
 
-    private AsyncRequestContent newContent(int size)
+    @ParameterizedTest
+    @MethodSource("formContentSizeScenarios")
+    public void testMaxFormContentSizeExceeded(Integer maxFormContentSize, boolean withContentLength) throws Exception
     {
+        start(handler ->
+        {
+            if (maxFormContentSize != null)
+                handler.setMaxFormContentSize(maxFormContentSize);
+            return new HttpServlet()
+            {
+                @Override
+                protected void service(HttpServletRequest request, HttpServletResponse response)
+                {
+                    request.getParameterMap();
+                }
+            };
+        });
+
         byte[] key = "foo=".getBytes(StandardCharsets.US_ASCII);
-        byte[] value = new byte[size - key.length];
+        int length = (maxFormContentSize == null || maxFormContentSize < 0)
+            ? ContextHandler.DEFAULT_MAX_FORM_CONTENT_SIZE
+            : maxFormContentSize;
+        // Avoid empty value.
+        length = length + 1;
+        byte[] value = new byte[length];
         Arrays.fill(value, (byte)'x');
-        return new AsyncRequestContent(ByteBuffer.wrap(key), ByteBuffer.wrap(value));
+        AsyncRequestContent content = new AsyncRequestContent(ByteBuffer.wrap(key), ByteBuffer.wrap(value));
+
+        ContentResponse response = client.newRequest("localhost", connector.getLocalPort())
+            .method(HttpMethod.POST)
+            .path(contextPath + servletPath)
+            .headers(headers -> headers.put(HttpHeader.CONTENT_TYPE, MimeTypes.Type.FORM_ENCODED.asString()))
+            .body(content)
+            .onRequestBegin(request ->
+            {
+                if (withContentLength)
+                    content.close();
+            })
+            .onRequestCommit(request ->
+            {
+                if (!withContentLength)
+                    content.close();
+            })
+            .send();
+
+        assertEquals(HttpStatus.BAD_REQUEST_400, response.getStatus());
     }
 
-    public static Stream<Integer> formKeysScenarios()
+    public static Stream<Arguments> formContentSizeScenarios()
     {
-        return Stream.of(null, -1, 0, MAX_FORM_KEYS);
+        return Stream.of(
+            Arguments.of(null, true),
+            Arguments.of(null, false),
+            Arguments.of(-1, true),
+            Arguments.of(-1, false),
+            Arguments.of(0, true),
+            Arguments.of(0, false),
+            Arguments.of(MAX_FORM_CONTENT_SIZE, true),
+            Arguments.of(MAX_FORM_CONTENT_SIZE, false)
+        );
     }
 
     @ParameterizedTest
