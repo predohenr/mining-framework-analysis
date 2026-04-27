ce6c4ca857683bfa19f3fa2895465e553e4dbdf6
Pedro Henrique <predohnr@gmail.com>
2026-04-27 10:56:24 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.java

--- modules/VisualizationEngine/src/main/java/org/gephi/viz/engine/jogl/models/EdgeLineModelUndirected.java
+++ modules/VisualizationEngine/src/main/java/org/gephi/viz/engine/jogl/models/EdgeLineModelUndirected.java
@@ -262,4 +262,18 @@ public class EdgeLineModelUndirected {
             programWithSelectionUnselected = null;
         }
     }
+
+    public static float[] getVertexData() {
+        //lineEnd, sideVector
+        return new float[] {
+            //Triangle 1
+            0, -1,// bottom left corner
+            1, -1,// top left corner
+            0, 1,// bottom right corner
+            //Triangle 2
+            0, 1,// bottom right corner
+            1, -1,// top left corner
+            1, 1// top right corner
+        };
+    }
 }
