6a73b89a142b0513010ac3c6cbbafb31262d728e
Pedro Henrique <predohnr@gmail.com>
2026-04-28 23:43:19 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.java

--- src/main/java/graphql/util/FpKit.java
+++ src/main/java/graphql/util/FpKit.java
@@ -18,6 +18,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.Set;
+import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
