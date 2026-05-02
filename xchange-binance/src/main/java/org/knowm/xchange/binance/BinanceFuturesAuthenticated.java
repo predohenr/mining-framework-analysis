9dd798cbbf5567a7ab0e1751f66513b403368c92
Pedro Henrique <predohnr@gmail.com>
2026-05-02 03:17:26 +0000

    Mining Framework Analysis for merge.mergiraf.java

--- xchange-binance/src/main/java/org/knowm/xchange/binance/BinanceFuturesAuthenticated.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/BinanceFuturesAuthenticated.java
@@ -16,16 +16,8 @@ import java.util.List;
 import org.knowm.xchange.binance.dto.BinanceException;
 import org.knowm.xchange.binance.dto.account.futures.BinanceFutureAccountInformation;
 import org.knowm.xchange.binance.dto.account.futures.BinanceFutureCommissionRate;
-import org.knowm.xchange.binance.dto.account.futures.BinancePosition;
 import org.knowm.xchange.binance.dto.trade.*;
-import org.knowm.xchange.binance.dto.trade.futures.BinanceChangeStatus;
-import org.knowm.xchange.binance.dto.trade.BinanceCancelledOrder;
-import org.knowm.xchange.binance.dto.trade.BinanceNewOrder;
-import org.knowm.xchange.binance.dto.trade.BinanceOrder;
-import org.knowm.xchange.binance.dto.trade.BinanceTrade;
-import org.knowm.xchange.binance.dto.trade.OrderSide;
-import org.knowm.xchange.binance.dto.trade.OrderType;
-import org.knowm.xchange.binance.dto.trade.TimeInForce;
+import org.knowm.xchange.binance.dto.account.futures.BinancePosition;
 import org.knowm.xchange.binance.dto.trade.futures.BinanceChangeStatus;
 import org.knowm.xchange.binance.dto.trade.futures.BinanceFutureNewOrder;
 import org.knowm.xchange.binance.dto.trade.futures.BinanceSetLeverage;
@@ -577,7 +569,7 @@ public interface BinanceFuturesAuthenticated extends BinanceFutures {
    */
   @DELETE
   @Path("fapi/v1/allOpenOrders")
-  List<BinanceCancelledOrder> cancelAllFutureOpenOrders(
+  BinanceChangeStatus cancelAllFutureOpenOrders(
       @QueryParam("symbol") String symbol,
       @QueryParam("recvWindow") Long recvWindow,
       @QueryParam("timestamp") SynchronizedValueFactory<Long> timestamp,
@@ -587,7 +579,6 @@ public interface BinanceFuturesAuthenticated extends BinanceFutures {
 
   /**
    * Get User Commission Rate for a Future symbol.
-   *
    * @param symbol
    * @param recvWindow optional
    * @param timestamp
