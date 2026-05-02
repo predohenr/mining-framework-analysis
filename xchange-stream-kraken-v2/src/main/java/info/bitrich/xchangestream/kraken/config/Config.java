e88bba609d3a5e32376a083843c40f481a464177
Pedro Henrique <predohnr@gmail.com>
2026-05-02 00:45:47 +0000

    Mining Framework Analysis for merge.mergiraf.java

--- xchange-stream-kraken-v2/src/main/java/info/bitrich/xchangestream/kraken/config/Config.java
+++ xchange-stream-kraken-v2/src/main/java/info/bitrich/xchangestream/kraken/config/Config.java
@@ -15,6 +15,7 @@ import java.util.Set;
 public final class Config {
 
   public static final Set<String> PRIVATE_CHANNELS = Set.of(ChannelType.BALANCES.getValue(), ChannelType.USER_TRADES.getValue());
+
   private ObjectMapper objectMapper;
   private Clock clock;
 
