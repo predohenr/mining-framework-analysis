0ad9fc15b1454feebdc50d3f1b6477a3f6ca6495
Pedro Henrique <predohnr@gmail.com>
2026-05-03 17:56:08 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.java

--- http-server-netty/src/main/java/io/micronaut/http/server/netty/NettyHttpServer.java
+++ http-server-netty/src/main/java/io/micronaut/http/server/netty/NettyHttpServer.java
@@ -16,6 +16,7 @@
 package io.micronaut.http.server.netty;
 
 import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.DefaultApplicationContext;
 import io.micronaut.context.BeanProvider;
 import io.micronaut.context.env.CachedEnvironment;
 import io.micronaut.context.env.Environment;
