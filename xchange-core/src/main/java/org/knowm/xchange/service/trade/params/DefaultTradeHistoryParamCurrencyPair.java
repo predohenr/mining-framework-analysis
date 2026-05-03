3e7f4d8077059f05c8f707fa2ca63272006e83f2
Pedro Henrique <predohnr@gmail.com>
2026-05-03 07:34:56 +0000

    Mining Framework Analysis for merge.mergiraf.java

--- xchange-core/src/main/java/org/knowm/xchange/service/trade/params/DefaultTradeHistoryParamCurrencyPair.java
+++ xchange-core/src/main/java/org/knowm/xchange/service/trade/params/DefaultTradeHistoryParamCurrencyPair.java
@@ -6,10 +6,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.knowm.xchange.currency.CurrencyPair;
 
+
+@NoArgsConstructor
+@Data
 @Data
 @Builder
-@AllArgsConstructor
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
+@AllArgsConstructor
 public class DefaultTradeHistoryParamCurrencyPair implements TradeHistoryParamCurrencyPair {
 
   private CurrencyPair currencyPair;
