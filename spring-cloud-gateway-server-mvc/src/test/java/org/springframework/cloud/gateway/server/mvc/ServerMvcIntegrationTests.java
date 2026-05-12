bf7dc15ac9f44936e0f9e57b0d627b4b8f6b33f6
Pedro Henrique <predohnr@gmail.com>
2026-05-12 13:55:05 +0000

    Mining Framework Analysis for merge.mergiraf.java

--- spring-cloud-gateway-server-mvc/src/test/java/org/springframework/cloud/gateway/server/mvc/ServerMvcIntegrationTests.java
+++ spring-cloud-gateway-server-mvc/src/test/java/org/springframework/cloud/gateway/server/mvc/ServerMvcIntegrationTests.java
@@ -66,8 +66,8 @@ import org.springframework.cloud.gateway.server.mvc.test.TestLoadBalancerConfig;
 import org.springframework.cloud.gateway.server.mvc.test.client.TestRestClient;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpEntity;
@@ -1032,7 +1032,7 @@ public class ServerMvcIntegrationTests {
 	@EnableAutoConfiguration
 	@LoadBalancerClient(name = "httpbin", configuration = TestLoadBalancerConfig.Httpbin.class)
 	@Import(PermitAllSecurityConfiguration.class)
-	protected static class TestConfiguration {
+	protected static class TestConfiguration extends WebMvcConfigurationSupport {
 
 		@Bean
 		StaticPortController staticPortController() {
@@ -1047,6 +1047,26 @@ public class ServerMvcIntegrationTests {
 		@Bean
 		EventController eventController() {
 			return new EventController();
+		}
+
+		// TODO This is needed to work around
+		// https://github.com/spring-cloud/spring-cloud-gateway/issues/3816
+		// which results from Spring Security being on the classpath. Once we can address
+		// this issue we should
+		// remove this bean and no longer extend WebMvcConfigurationSupport in this
+		// configuration class
+		@Bean
+		@Lazy
+		@Override
+		public HandlerMappingIntrospector mvcHandlerMappingIntrospector() {
+			return new HandlerMappingIntrospector() {
+				@Override
+				public Filter createCacheFilter() {
+					return (request, response, chain) -> {
+						chain.doFilter(request, response);
+					};
+				}
+			};
 		}
 
 		@Bean
