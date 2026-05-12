aa3a378cdb94514a7e6b7c7b9fa3cadbdb78d8db
Pedro Henrique <predohnr@gmail.com>
2026-05-12 13:15:32 +0000

    Mining Framework Analysis for merge.mergiraf.java

--- spring-cloud-kubernetes-fabric8-leader/src/main/java/org/springframework/cloud/kubernetes/fabric8/leader/election/Fabric8LeaderElectionAutoConfiguration.java
+++ spring-cloud-kubernetes-fabric8-leader/src/main/java/org/springframework/cloud/kubernetes/fabric8/leader/election/Fabric8LeaderElectionAutoConfiguration.java
@@ -107,8 +107,7 @@ class Fabric8LeaderElectionAutoConfiguration {
 
 	@Bean
 	@ConditionalOnMissingBean
-	Lock fabric8LeaderElectionLock(KubernetesClient fabric8KubernetesClient, LeaderElectionProperties properties,
-			String candidateIdentity) {
+	Lock fabric8LeaderElectionLock(KubernetesClient fabric8KubernetesClient, LeaderElectionProperties properties, String candidateIdentity) {
 		boolean leaseSupported = fabric8KubernetesClient.getApiGroups()
 			.getGroups()
 			.stream()
