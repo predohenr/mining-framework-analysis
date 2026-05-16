03e29d6ca0f6bb07c580e0e97817fb9cc4e0d743
Pedro Henrique <predohnr@gmail.com>
2026-05-16 13:21:12 +0000

    Mining Framework Analysis for merge.mergiraf.java

--- oauth2/oauth2-client/src/main/java/org/springframework/security/oauth2/client/oidc/authentication/OidcAuthorizedClientRefreshedEventListener.java
+++ oauth2/oauth2-client/src/main/java/org/springframework/security/oauth2/client/oidc/authentication/OidcAuthorizedClientRefreshedEventListener.java
@@ -319,9 +319,8 @@ public final class OidcAuthorizedClientRefreshedEventListener
 		// The auth_time claim MUST represent the time of the original authentication OR
 		// the most recent time when the end-user reauthenticated when "prompt=login" is
 		// passed in the authentication request
-		if (!idToken.getAuthenticatedAt().equals(existingOidcUser.getIdToken().getAuthenticatedAt())
-				&& (existingOidcUser.getIdToken().getAuthenticatedAt() == null
-						|| !idToken.getAuthenticatedAt().isAfter(existingOidcUser.getIdToken().getAuthenticatedAt()))) {
+		if (!Objects.equals(idToken.getAuthenticatedAt(), existingOidcUser.getIdToken().getAuthenticatedAt())
+				&& !idToken.getAuthenticatedAt().isAfter(existingOidcUser.getIdToken().getAuthenticatedAt())) {
 			OAuth2Error oauth2Error = new OAuth2Error(INVALID_ID_TOKEN_ERROR_CODE, "Invalid authenticated at time",
 					REFRESH_TOKEN_RESPONSE_ERROR_URI);
 			throw new OAuth2AuthenticationException(oauth2Error, oauth2Error.toString());
