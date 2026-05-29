732e063bc220c61f12d86bbb595750b9f265427e
Pedro Henrique <predohnr@gmail.com>
2026-05-29 07:08:03 +0000

    Mining Framework Analysis for merge.diff3.java

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/rest/TbRestApiCallNode.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/rest/TbRestApiCallNode.java
@@ -26,7 +26,7 @@ import org.thingsboard.rule.engine.external.TbAbstractExternalNode;
 import org.thingsboard.server.common.data.plugin.ComponentType;
 import org.thingsboard.server.common.data.util.TbPair;
 import org.thingsboard.server.common.msg.TbMsg;
-import org.thingsboard.server.exception.DataValidationException;
+import org.thingsboard.server.dao.exception.DataValidationException;
 
 import java.util.Iterator;
 import java.util.List;
