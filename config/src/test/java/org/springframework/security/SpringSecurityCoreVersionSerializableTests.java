786aa807612a0dfa7cb0fbed7ee36dfac50891b4
Pedro Henrique <predohnr@gmail.com>
2026-06-05 05:58:24 +0000

    Mining Framework Analysis for merge.mergiraf.java

--- config/src/test/java/org/springframework/security/SpringSecurityCoreVersionSerializableTests.java
+++ config/src/test/java/org/springframework/security/SpringSecurityCoreVersionSerializableTests.java
@@ -45,8 +45,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import jakarta.servlet.http.Cookie;
 import org.apache.commons.lang3.ObjectUtils;
+import jakarta.servlet.http.Cookie;
 import org.apereo.cas.client.validation.AssertionImpl;
 import org.instancio.Instancio;
 import org.instancio.InstancioApi;
@@ -206,6 +206,7 @@ import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.security.web.savedrequest.SimpleSavedRequest;
 import org.springframework.security.web.server.firewall.ServerExchangeRejectedException;
 import org.springframework.security.web.session.HttpSessionCreatedEvent;
+import org.springframework.security.web.util.UrlUtils;
 import org.springframework.security.web.webauthn.api.Bytes;
 import org.springframework.security.web.webauthn.api.ImmutablePublicKeyCredentialUserEntity;
 import org.springframework.security.web.webauthn.api.PublicKeyCredentialUserEntity;
