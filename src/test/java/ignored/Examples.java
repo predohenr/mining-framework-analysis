bf21c34aaf2c80155694c0108be576ed35f92fe4
Pedro Henrique <predohnr@gmail.com>
2026-06-06 12:37:47 +0000

    Mining Framework Analysis for merge.mergiraf.java

--- src/test/java/ignored/Examples.java
+++ src/test/java/ignored/Examples.java
@@ -67,14 +67,15 @@ public class Examples extends AcceptanceTestBase {
   @Test
   public void queryMethod() {
     stubFor(
-        query(urlEqualTo("/some/thing"))
-            .withRequestBody(matching("<status>OK</status>"))
-            .willReturn(
-                aResponse().withHeader("Content-Type", "text/plain").withBody("Hello world!")));
+            query(urlEqualTo("/some/thing"))
+                    .withRequestBody(matching("<status>OK</status>"))
+                    .willReturn(
+                            aResponse()
+                                    .withHeader("Content-Type", "text/plain")
+                                    .withBody("Hello world!")));
 
     assertThat(testClient.queryXml("/some/thing", "<status>OK</status>").statusCode(), is(200));
-    assertThat(
-        testClient.queryXml("/some/thing", "<status>OK</status>").content(), is("Hello World!"));
+    assertThat(testClient.queryXml("/some/thing", "<status>OK</status>").content(), is("Hello World!"));
     assertThat(testClient.query("/some/thing/else").statusCode(), is(404));
   }
 
