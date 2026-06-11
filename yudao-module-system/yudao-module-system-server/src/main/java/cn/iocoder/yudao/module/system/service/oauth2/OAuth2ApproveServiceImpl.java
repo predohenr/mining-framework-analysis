bfbed7dac711a41ec65834f827dfc5a5265027cf
Pedro Henrique <predohnr@gmail.com>
2026-06-11 11:41:16 +0000

    Mining Framework Analysis for merge.mergiraf.java

--- yudao-module-system/yudao-module-system-server/src/main/java/cn/iocoder/yudao/module/system/service/oauth2/OAuth2ApproveServiceImpl.java
+++ yudao-module-system/yudao-module-system-server/src/main/java/cn/iocoder/yudao/module/system/service/oauth2/OAuth2ApproveServiceImpl.java
@@ -7,16 +7,16 @@ import cn.iocoder.yudao.module.system.dal.dataobject.oauth2.OAuth2ApproveDO;
 import cn.iocoder.yudao.module.system.dal.dataobject.oauth2.OAuth2ClientDO;
 import cn.iocoder.yudao.module.system.dal.mysql.oauth2.OAuth2ApproveMapper;
 import com.google.common.annotations.VisibleForTesting;
-import jakarta.annotation.Resource;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.annotation.Validated;
-
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
+import org.springframework.transaction.annotation.Transactional;
+import javax.annotation.Resource;
+import jakarta.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.Set;
+import java.util.Collection;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
 import static cn.iocoder.yudao.framework.common.util.collection.CollectionUtils.convertSet;
 
