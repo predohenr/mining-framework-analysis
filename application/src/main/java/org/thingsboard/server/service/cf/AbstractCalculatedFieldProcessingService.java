b1a6a54bd7f65028f6e37f0a9e4f5e5162ecdf98
Pedro Henrique <predohnr@gmail.com>
2026-06-14 05:32:25 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.java

--- application/src/main/java/org/thingsboard/server/service/cf/AbstractCalculatedFieldProcessingService.java
+++ application/src/main/java/org/thingsboard/server/service/cf/AbstractCalculatedFieldProcessingService.java
@@ -62,8 +62,8 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
-import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static org.thingsboard.server.common.data.cf.CalculatedFieldType.PROPAGATION;
