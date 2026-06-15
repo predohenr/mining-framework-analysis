fb8aca2590b0f650afa441062e850edd983001f0
Pedro Henrique <predohnr@gmail.com>
2026-06-15 01:11:23 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.java

--- application/src/main/java/org/thingsboard/server/service/cf/ctx/state/SimpleCalculatedFieldState.java
+++ application/src/main/java/org/thingsboard/server/service/cf/ctx/state/SimpleCalculatedFieldState.java
@@ -19,15 +19,16 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
-import lombok.EqualsAndHashCode;
-import net.objecthunter.exp4j.Expression;
-import org.thingsboard.common.util.JacksonUtil;
-import org.thingsboard.common.util.NumberUtils;
-import org.thingsboard.server.actors.TbActorRef;
 import org.thingsboard.server.common.data.cf.CalculatedFieldType;
 import org.thingsboard.server.common.data.cf.configuration.Output;
-import org.thingsboard.server.common.data.id.EntityId;
 import org.thingsboard.server.service.cf.CalculatedFieldResult;
+import org.thingsboard.common.util.JacksonUtil;
+import org.thingsboard.server.common.data.cf.configuration.OutputType;
+import org.thingsboard.server.actors.TbActorRef;
+import net.objecthunter.exp4j.Expression;
+import org.thingsboard.common.util.NumberUtils;
+import lombok.EqualsAndHashCode;
+import org.thingsboard.server.common.data.id.EntityId;
 import org.thingsboard.server.service.cf.TelemetryCalculatedFieldResult;
 
 import java.util.Map;
@@ -35,16 +36,69 @@ import java.util.Map;
 @EqualsAndHashCode(callSuper = true)
 public class SimpleCalculatedFieldState extends BaseCalculatedFieldState {
 
-    private ThreadLocal<Expression> expression;
-
-    public SimpleCalculatedFieldState(EntityId entityId) {
-        super(entityId);
+    @Override
+    public CalculatedFieldType getType() {
+        return CalculatedFieldType.SIMPLE;
     }
 
     @Override
-    public void setCtx(CalculatedFieldCtx ctx, TbActorRef actorCtx) {
-        super.setCtx(ctx, actorCtx);
-        this.expression = ctx.getSimpleExpressions().get(ctx.getExpression());
+    public ListenableFuture<CalculatedFieldResult> performCalculation(CalculatedFieldCtx ctx) {
+        var expr = ctx.getCustomExpression().get();
+
+        for (Map.Entry<String, ArgumentEntry> entry : this.arguments.entrySet()) {
+            try {
+                BasicKvEntry kvEntry = ((SingleValueArgumentEntry) entry.getValue()).getKvEntryValue();
+                double value = switch (kvEntry.getDataType()) {
+                    case LONG -> kvEntry.getLongValue().map(Long::doubleValue).orElseThrow();
+                    case DOUBLE -> kvEntry.getDoubleValue().orElseThrow();
+                    case BOOLEAN -> kvEntry.getBooleanValue().map(b -> b ? 1.0 : 0.0).orElseThrow();
+                    case STRING, JSON -> Double.parseDouble(kvEntry.getValueAsString());
+                };
+                expr.setVariable(entry.getKey(), value);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Argument '" + entry.getKey() + "' is not a number.");
+            }
+        }
+
+        double expressionResult = expr.evaluate();
+
+        Output output = ctx.getOutput();
+        Object result = formatResult(expressionResult, output.getDecimalsByDefault());
+        JsonNode outputResult = createResultJson(ctx, result);
+
+        return Futures.immediateFuture(new CalculatedFieldResult(output.getType(), output.getScope(), outputResult));
+    }
+
+    private JsonNode createResultJson(CalculatedFieldCtx ctx, Object result) {
+        Output output = ctx.getOutput();
+        String outputName = output.getName();
+        ObjectNode valuesNode = JacksonUtil.newObjectNode();
+        if (result instanceof Double doubleValue) {
+            valuesNode.put(outputName, doubleValue);
+        } else if (result instanceof Integer integerValue) {
+            valuesNode.put(outputName, integerValue);
+        } else {
+            valuesNode.set(outputName, JacksonUtil.valueToTree(result));
+        }
+        if (output.getType() == OutputType.ATTRIBUTES || !ctx.isUseLatestTs()) {
+            return valuesNode;
+        }
+        long latestTs = getLatestTimestamp();
+        if (latestTs == DEFAULT_LAST_UPDATE_TS) {
+            return valuesNode;
+        }
+        ObjectNode resultNode = JacksonUtil.newObjectNode();
+        resultNode.put("ts", latestTs);
+        resultNode.set("values", valuesNode);
+        return resultNode;
+    }
+
+    @Override
+    protected void validateNewEntry(String key, ArgumentEntry newEntry) {
+        if (newEntry instanceof TsRollingArgumentEntry) {
+            throw new IllegalArgumentException("Unsupported argument type detected for argument: " + key + ". " +
+                                               "Rolling argument entry is not supported for simple calculated fields.");
+        }
     }
 
     @Override
@@ -63,6 +117,12 @@ public class SimpleCalculatedFieldState extends BaseCalculatedFieldState {
                 .build());
     }
 
+    private ThreadLocal<Expression> expression;
+
+    public SimpleCalculatedFieldState(EntityId entityId) {
+        super(entityId);
+    }
+
     private JsonNode createResultJson(String outputName, Object result) {
         ObjectNode valuesNode = JacksonUtil.newObjectNode();
         if (result instanceof Double doubleValue) {
@@ -76,16 +136,9 @@ public class SimpleCalculatedFieldState extends BaseCalculatedFieldState {
     }
 
     @Override
-    protected void validateNewEntry(String key, ArgumentEntry newEntry) {
-        if (newEntry instanceof TsRollingArgumentEntry) {
-            throw new IllegalArgumentException("Unsupported argument type detected for argument: " + key + ". " +
-                                               "Rolling argument entry is not supported for simple calculated fields.");
-        }
-    }
-
-    @Override
-    public CalculatedFieldType getType() {
-        return CalculatedFieldType.SIMPLE;
+    public void setCtx(CalculatedFieldCtx ctx, TbActorRef actorCtx) {
+        super.setCtx(ctx, actorCtx);
+        this.expression = ctx.getSimpleExpressions().get(ctx.getExpression());
     }
 
 }
