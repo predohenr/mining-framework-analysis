242317983e9861607bc5037de6c2c087b2b4da35
Pedro Henrique <predohnr@gmail.com>
2026-06-16 13:47:46 +0000

    Mining Framework Analysis for merge.mergiraf.java

--- yudao-module-crm/yudao-module-crm-server/src/main/java/cn/iocoder/yudao/module/crm/service/receivable/listener/CrmReceivableStatusListener.java
+++ yudao-module-crm/yudao-module-crm-server/src/main/java/cn/iocoder/yudao/module/crm/service/receivable/listener/CrmReceivableStatusListener.java
@@ -6,11 +6,16 @@ import cn.iocoder.yudao.module.bpm.api.event.BpmProcessInstanceStatusEventListen
 import cn.iocoder.yudao.module.crm.enums.ApiConstants;
 import cn.iocoder.yudao.module.crm.service.receivable.CrmReceivableService;
 import cn.iocoder.yudao.module.crm.service.receivable.CrmReceivableServiceImpl;
-import jakarta.annotation.Resource;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
 import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.RestController;
 
 /**
