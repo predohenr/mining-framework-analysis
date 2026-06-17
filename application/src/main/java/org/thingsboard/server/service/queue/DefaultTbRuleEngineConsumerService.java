116306afc5155bcf3aee39c365d38f67496e8cae
Pedro Henrique <predohnr@gmail.com>
2026-06-17 03:30:32 +0000

    Mining Framework Analysis for merge.mergiraf_semi.java

--- application/src/main/java/org/thingsboard/server/service/queue/DefaultTbRuleEngineConsumerService.java
+++ application/src/main/java/org/thingsboard/server/service/queue/DefaultTbRuleEngineConsumerService.java
@@ -84,9 +84,29 @@ public class DefaultTbRuleEngineConsumerService extends AbstractPartitionBasedCo
                                               TbApiUsageStateService apiUsageStateService,
                                               PartitionService partitionService,
                                               ApplicationEventPublisher eventPublisher,
-                                              JwtSettingsService jwtSettingsService,
-                                              TbMsgPackProcessingContextFactory packProcessingContextFactory) {
+                                              JwtSettingsService jwtSettingsService) {
         super(actorContext, tenantProfileCache, deviceProfileCache, assetProfileCache, tbResourceDataCache, apiUsageStateService, partitionService, eventPublisher, jwtSettingsService);
+        this.ctx = ctx;
+        this.tbDeviceRpcService = tbDeviceRpcService;
+        this.queueService = queueService;
+        this.packProcessingContextFactory = packProcessingContextFactory;
+    }
+
+    public DefaultTbRuleEngineConsumerService(TbRuleEngineConsumerContext ctx,
+                                              ActorSystemContext actorContext,
+                                              TbRuleEngineDeviceRpcService tbDeviceRpcService,
+                                              QueueService queueService,
+                                              TbDeviceProfileCache deviceProfileCache,
+                                              TbAssetProfileCache assetProfileCache,
+                                              TbResourceDataCache tbResourceDataCache,
+                                              TbTenantProfileCache tenantProfileCache,
+                                              TbApiUsageStateService apiUsageStateService,
+                                              PartitionService partitionService,
+                                              ApplicationEventPublisher eventPublisher,
+                                              JwtSettingsService jwtSettingsService,
+                                              CalculatedFieldCache calculatedFieldCache,
+                                              TbMsgPackProcessingContextFactory packProcessingContextFactory) {
+        super(actorContext, tenantProfileCache, deviceProfileCache, assetProfileCache, tbResourceDataCache, calculatedFieldCache, apiUsageStateService, partitionService, eventPublisher, jwtSettingsService);
         this.ctx = ctx;
         this.tbDeviceRpcService = tbDeviceRpcService;
         this.queueService = queueService;
