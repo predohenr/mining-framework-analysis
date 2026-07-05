c15365e507d0a63684b081866a1e9c3021c83635
Pedro Henrique <predohnr@gmail.com>
2026-07-05 11:43:18 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.java

--- yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/controller/admin/socail/SocialUserController.java
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/controller/admin/socail/SocialUserController.java
@@ -14,16 +14,16 @@ import cn.iocoder.yudao.module.system.service.social.SocialUserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.annotation.Resource;
-import jakarta.validation.Valid;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
 import static cn.iocoder.yudao.framework.common.util.collection.CollectionUtils.convertList;
+import jakarta.validation.Valid;
+import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import org.springframework.security.access.prepost.PreAuthorize;
+import jakarta.annotation.Resource;
+import javax.annotation.Resource;
+import javax.validation.Valid;
 import static cn.iocoder.yudao.framework.security.core.util.SecurityFrameworkUtils.getLoginUserId;
 
 @Tag(name = "管理后台 - 社交用户")
