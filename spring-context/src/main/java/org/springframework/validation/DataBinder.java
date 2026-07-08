b56677c1d07e9b259be76bed2137111f84b04e48
Pedro Henrique <predohnr@gmail.com>
2026-07-08 23:47:25 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.java

--- spring-context/src/main/java/org/springframework/validation/DataBinder.java
+++ spring-context/src/main/java/org/springframework/validation/DataBinder.java
@@ -1106,7 +1106,8 @@ public class DataBinder implements PropertyEditorRegistry, TypeConverter {
 	}
 
 	@SuppressWarnings("unchecked")
-	private <V> @Nullable V createIndexedValue(
+	@Nullable
+	private <V> V createIndexedValue(
 			String paramPath, Class<?> paramType, ResolvableType elementType,
 			String indexedPath, ValueResolver valueResolver) {
 
